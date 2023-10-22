80949115eb107a03fd12e7aa9ff272b0625cea1e
Damir <dense.neer@gmail.com>
2023-10-22 19:22:40 +0600

    change header, few behaivors for to-list

--- missionsScript.js
+++ missionsScript.js
@@ -1,37 +1,74 @@
 //missions.html page
-var inputBox = document.getElementById("input-box");
+var title = document.getElementById("title");
+var decribe = document.getElementById("desc");
+var level = document.getElementById("level");
+var impt = document.getElementById("impt");
+
 var listContainer = document.getElementById("list-container");
 
-function addMission(){
-    if(inputBox.value === ''){
+function addMission() {
+    if (title.value === '') {
         alert("Your input place is empty. To add mission need to decribe it!")
     } else {
         console.log("Da")
         let li = document.createElement("li");
-        li.innerHTML = '<i class="fa-regular fa-circle"></i>' + inputBox.value;
+        li.innerHTML = '<i class="fa-regular fa-circle" onclick="missionComplete(this)" onmouseover="checkMouseOver(this)" onmouseout="checkMouseOut(this)" ></i><div class="card bg-dark text-white p-4" onclick="openTimer(this)"><h5 class="card-title" style="font-size:3rem">' + title.value + '</h5><h6 class="card-subtitle mb-2  text-white-50">' + decribe.value + '</h6><p class="card-text" style="font-size:1rem">Level: ' + level.value + '</p><p class="card-text" style="font-size:1rem">Level: ' + impt.value + '</p></div>';
         listContainer.appendChild(li);
         let span = document.createElement("span");
-        localStorage.setItem("mission", JSON.stringify([...JSON.parse(localStorage.getItem("mission") || "[]"), {mission: inputBox.value, completed: false}]))
+        localStorage.setItem("ms", JSON.stringify([...JSON.parse(localStorage.getItem("ms") || "[]"), { title: title.value, decribe: decribe.value, level: level.value, impt: impt.value, completed: false }]));
+        // saveMissions();
     }
-    inputBox.value = '';
+    title.value = '';
+    decribe.value = '';
+    level.value = "Underfined";
+    impt.value = "Underfined";
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }
-}, false);
+function openTimer(div){
+    window.open("index.html");
+}
 
 
 
 window.onload = loadMissions;
 
+// function loadMissions() {
+//     listContainer.innerHTML = localStorage.getItem("missions")
+// }
 function loadMissions() {
-    let missions = Array.from(JSON.parse(localStorage.getItem("mission")));
 
-    missions.forEach(mission => {
+    let mis = Array.from(JSON.parse(localStorage.getItem("ms")));
+
+    // Loop through the tasks and add them to the list
+    mis.forEach(ms => {
         const li = document.createElement("li");
-        li.innerHTML = '<i class="fa-regular fa-circle"></i>' + mission.mission;
-        listContainer.insertBefore(li, listContainer[0]);
-    })
+        li.innerHTML = '<div class="preCard"><i class="fa-regular fa-circle" onclick="missionComplete(this)" onmouseover="checkMouseOver(this)" onmouseout="checkMouseOut(this)" ></i></div><div class="card bg-dark text-white p-4" onclick="openTimer(this)"><h5 class="card-title" style="font-size:3rem">' + ms.title + '</h5><h6 class="card-subtitle mb-2 text-white-50">' + ms.decribe + '</h6><p class="card-text" style="font-size:1rem">Level: ' + ms.level + '</p><p class="card-text" style="font-size:1rem">Level: ' + ms.impt + '</p></div>';
+        listContainer.insertBefore(li, listContainer.children[0]);
+    });
+}
+function saveMissions() {
+    localStorage.setItem("missions", listContainer.innerHTML)
+}
+function reset() {
+    localStorage.clear();
+    location.reload();
+}
+
+function missionComplete(object) {
+    if (object.classList.contains('fa-circle-check')) {
+        object.classList.remove("fa-circle-check");
+        object.classList.add("fa-circle");
+    } else {
+        object.classList.remove("fa-circle");
+        object.classList.add("fa-circle-check");
+    }
+    saveMissions();
+}
+function checkMouseOver(object) {
+    object.classList.remove("fa-regural");
+    object.classList.add("fa-solid");
+}
+function checkMouseOut(object) {
+    object.classList.remove("fa-solid");
+    object.classList.add("fa-regular");
 }
